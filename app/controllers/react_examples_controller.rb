5eed9084b19bb1a2ae2518b6e8aae4b4632ef669
Ambey Gupta <ambeyyuvasoft137@gmail.com>
2016-11-25 19:28:17 +0530

    changes in home page

--- app/controllers/react_examples_controller.rb
+++ app/controllers/react_examples_controller.rb
@@ -1,6 +1,6 @@
 class ReactExamplesController < ApplicationController
-	before_action :authenticate_user!
-  def component
-    @name = 'test'
-  end
+	# before_action :authenticate_user!
+ #  def component
+ #    @name = 'test'
+ #  end
 end
